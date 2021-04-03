70b203f4c362d81d841a95f36d2e780fd72851dc
Max Faupel <max.faupel@gmx.de>
2021-04-03 18:29:58 +0200

    implement simple login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = ''
+  password = ''
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin() {
+    console.log(this.username);
+    if (this.username==='max' && this.password==='dummy') {
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
   }
 
 }
