f30ff7d152c36be3a9578cb2b7b2f604ec39c380
Max Faupel <max.faupel@gmx.de>
2021-04-07 19:34:37 +0200

    get welcome message from REST service

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export class WelcomeBean {
+  constructor(
+    public message: string
+  ) { }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getWelcomeMessage(): Observable<WelcomeBean> {
+    return this.httpClient.get<WelcomeBean>('http://localhost:8080/welcome')
+  }
+
+  getWelcomeMessageForUser(username: string): Observable<WelcomeBean> {
+    return this.httpClient.get<WelcomeBean>(`http://localhost:8080/welcome/${username}`)
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeBean, WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -8,13 +9,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
 
-  name = ''
+  username = ''
+  message: string = ''
+  errorMessage: string = ''
 
-  constructor(private activatedRoute : ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private service: WelcomeDataService) { }
 
   ngOnInit(): void {
-    this.name = this.activatedRoute.snapshot.params['name']
-    console.log(this.name)
+    this.username = this.activatedRoute.snapshot.params['name']
+    console.log(this.username)
   }
 
+  getWelcomeMessage() {
+    this.service.getWelcomeMessage().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+
+  getWelcomeMessageForUser() {
+    this.service.getWelcomeMessageForUser(this.username).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+
+  handleSuccessfulResponse(response: WelcomeBean) {
+    this.message = response.message
+  }
+
+  handleErrorResponse(error: any): void {
+    this.errorMessage = error.error.message
+  }
 }
